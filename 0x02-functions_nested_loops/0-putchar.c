ba4ae68493938a1483a21a5b9abb650ca7e256e2
alfredirimaso <alfredirimaso2020@gmail.com>
2023-03-21 08:46:40 -0700

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,19 @@
-#include<unistd.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
 
-int _putchar(char c){
-	return (write(1, &c, 1));
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
