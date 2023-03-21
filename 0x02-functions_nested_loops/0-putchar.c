90d7fd37b21b22c60523f726b04514a9cf2f59b7
alfredirimaso <alfredirimaso2020@gmail.com>
2023-03-21 02:42:14 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,5 @@
-#include "stdio.h"
+#include<unistd.h>
 
-/**
- * main - prints _putchar
- * Return: 0 (Success)
- */
-
-int main(void)
-{
-	char text[10] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+int _putchar(char c){
+	return (write(1, &c, 1));
 }
