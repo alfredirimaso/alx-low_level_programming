b673c3172fd49d8d697c52af5add73f99ccd631a
alfredirimaso <alfredirimaso2020@gmail.com>
2023-03-21 02:24:09 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - prints *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
