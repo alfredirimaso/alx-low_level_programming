32cac4c19210dcc94da36ec55b6b72f89dacb31a
alfredirimaso <alfredirimaso2020@gmail.com>
2023-04-28 10:21:11 -0700

    more_singly_linked_list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop contained in
+ *                     a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: If there is no loop - NULL.
+ *         Otherwise - the address of the node where the loop starts.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,22 @@
+0x13. C - More singly linked lists
+C
+Algorithm
+Data structure
+ By: Julien Barbier
+ Weight: 1
+ Project will start Apr 28, 2023 6:00 AM, must end by May 3, 2023 6:00 AM
+ Checker will be released at Apr 29, 2023 12:00 PM
+ An auto review will be launched at the deadline
+
+
+esources
+Read or watch:
+
+Google
+Youtube
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+How to use linked lists
+Start to look for the right source of information without too much help
